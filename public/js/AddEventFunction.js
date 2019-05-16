4bd0670cdb05c416e7712e508d35b0298e898e95
Philip Bolinao <pdbolinao@gmail.com>
2019-05-15 19:08:47 -0700

    new calendar css

---
+++ public/js/AddEventFunction.js
@@ -0,0 +1,21 @@
+function writeNewPost(uid, date, title, body) {
+    // A post entry.
+    var postData = {
+      author: username,
+      uid: uid,
+      body: body,
+      title: title,
+      starCount: 0,
+      authorPic: picture
+    };
+  
+    // Get a key for a new Post.
+    var newPostKey = firebase.database().ref().child('posts').push().key;
+  
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/posts/' + newPostKey] = postData;
+    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+  
+    return firebase.database().ref().update(updates);
+  }
