caa82adc46436a9887de6e243446aed4ee59b1e2
Kibum Park <parkkibs@gmail.com>
2019-05-17 11:59:38 -0700

    Added Database Writing Function

--- public/js/events.js
+++ public/js/events.js
@@ -1,15 +1,47 @@
-
+//Function to turn Form Data into JSON
+var arraylist = {}
 $("#forms").submit(function(event) {
-  console.log($(this).serializeArray());
+  arraylist = $(this).serializeArray()
+  console.log(arraylist);
   event.preventDefault();
+
+  var firebase = app_firebase;  
+  var dB = firebase.database();
+  var userId = firebase.auth().currentUser.uid;
+  var str = "riuehkjsnrandomstringsjk"
+
+  dB.ref('users/' + userId + '/eventsCreated').set({
+    1: str
+  })
+  dB.ref('events/' + str).set({
+    year: "2019",
+    month: "May",
+    day: "17",
+    eventname: arraylist[0].value,
+    eventStartTime: arraylist[1].value,
+    eventEndTime: arraylist[2].value,
+    eventDetails: arraylist[3].value,
+  })
+  
 });
 
-formArray = {}
-function objectifyForm(formArray) {//serialize data function
+var myJSON = JSON.stringify(arraylist);
+console.log(typeof arraylist);
 
-    var returnArray = {};
-    for (var i = 0; i < formArray.length; i++){
-      returnArray[formArray[i]['name']] = formArray[i]['value'];
+//Function for Event Panel Sliding Animation
+var trigbutton = $('#trigger');
+$(trigbutton).click(function (event) {
+  var menu = $('#eventpanel');
+  var showMenu = false;
+  var menuAnimSlideIn = {"right": "-400", "easing": "swing"};
+  var menuAnimSlideOut = {"right": "0", "easing": "swing"};
+    if (!showMenu) {
+        showMenu = true;
+        $(menu).animate(menuAnimSlideIn, 100);
+    } else {
+        showMenu = false;
+        $(menu).animate(menuAnimSlideOut, 100);
     }
-    return returnArray;
-  }
+});
+
+var dates = $("#dateInput").value
