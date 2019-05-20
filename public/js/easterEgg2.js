7aef3eaabd5932eef7ac881bc2630116adee0b4a
Philip Bolinao <pdbolinao@gmail.com>
2019-05-20 01:16:31 -0700

    added a dumb easter egg for jokes

---
+++ public/js/easterEgg2.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    let count = 0;
+
+    $("#easteregg2").click(function () {
+        count++;
+        console.log(count)
+                
+        if (count===3) {
+            $('#story').css("background-image", "url('../static/images/endgame.jpg')")
+            $('#story').css("background-size", "cover")
+        }
+        if (count ==6) {
+            $('#story').css("background-image", "none")
+            $('#story').css("background-size", "cover")
+            count = 0;
+        }
+    })
+})
+
