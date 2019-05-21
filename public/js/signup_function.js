efb764ebb22703324fc207a98dc2d747f02ef0e2
Amarpreet Singara <amarsingara@hotmail.com>
2019-05-20 22:00:59 -0700

    updated

--- public/js/signup_function.js
+++ public/js/signup_function.js
@@ -19,7 +19,7 @@ var uiConfig = {
   signInSuccessUrl: 'login.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
@@ -74,3 +74,25 @@ function signup() {
   document.getElementById("login_div").style.display = "none";
 }
 
+function googleLogin(){
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(hello);
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+}
